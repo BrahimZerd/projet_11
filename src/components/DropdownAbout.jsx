b38d1325c26ad768072d10b74f3a54707c8c7820
Brahim Zerd <brahim.zerd@live.fr>
2022-12-23 09:20:07 +0100

    Reste navigate

--- src/components/DropdownAbout.jsx
+++ src/components/DropdownAbout.jsx
@@ -38,6 +38,7 @@ padding-bottom: 14px;`
 
 
 function Dropdown(props) {
+  console.log(props)
     const [open, setOpen] = React.useState(false);
     
     
@@ -62,20 +63,7 @@ function Dropdown(props) {
     )
 
 }
-function AllDropdowns() {
-    return (
-      <div>
-        <Dropdown name="Fiabilite" description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.
-"  id="rotate0"/>
-        <Dropdown name="Respect" description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.
-" id="rotate1"/>
-        <Dropdown name="Service" description="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.
-" id="rotate2"/>
-        <Dropdown name="Securite"  description="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.
-" id="rotate3" className= "dropdownLast"/>
-      </div>
-    );
-  }
 
 
-export default AllDropdowns;
+
+export default Dropdown;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import Layout from '../components/LayoutHome';
 import '../App.css';
 import styled from 'styled-components';
 import CardList from '../components/Card';
+import data from '../assets/data.json'
+import { useParams } from 'react-router-dom';
 
 
 const BodyDiv = styled.div`
@@ -19,7 +21,13 @@ function Home() {
       
     <Layout />
     
-    <BodyDiv >
+    <BodyDiv style={{display: "grid",gridTemplateColumns: "repeat(3 , 1fr)",
+    }}>
+      
+      {data.map((object) => (
+        
+        <CardList {...object} key={data.indexOf(object)}/>
+      ))}
     <CardList />
     </BodyDiv>
     </div>
